325850b04910c2d47393fdf8651f1e01d6ba5f0a
doruletzz <doros.doru@yahoo.com>
2022-03-23 17:25:33 +0200

    added section styles

--- src/components/blog/BlogPostCard.tsx
+++ src/components/blog/BlogPostCard.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { useAppSelector } from '../../redux/app/hooks';
 
-const BlogPostCard = ({title, summary, date, time}) => {
+import styles from './BlogPostCard.module.scss';
+
+
+//TODO: add param type
+const BlogPostCard = ({ title, summary, date, time }) => {
+
+  const { isDark } = useAppSelector(state => state.theme);
+
   return (
-    <Card>
-        <Card.Header>
-            <Card.Title>{title}</Card.Title>
-            <Card.Subtitle>{date} - {time}</Card.Subtitle>
-        </Card.Header>
-        <Card.Body>
-            <Card.Text>{summary}</Card.Text>
-        </Card.Body>
+    <div className={isDark ? styles.theme__dark : styles.theme__light}>
+      <Card className={styles.blog_post_card}>
 
-    </Card>
+        <Card.Body>
+          <Card.Title className={styles.title}>{title}</Card.Title>
+          <Card.Subtitle className={styles.subtitle}>{date} - {time}</Card.Subtitle>
+          <Card.Text className={styles.content}>{summary}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
--- src/pages/landing/BlogSection.tsx
+++ src/pages/landing/BlogSection.tsx
@@ -7,41 +7,50 @@ import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
 import { fetchLatestPosts } from '../../redux/blog/slice';
 
 
+import styles from './BlogSection.module.scss'
+
 const BlogSection = () => {
 
   const nrOfBlogPostCards = 4;
 
-  const {posts} = useAppSelector(state => state.posts);
+  const { posts } = useAppSelector(state => state.posts);
+
+  const { isDark } = useAppSelector(state => state.theme);
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-      dispatch(fetchLatestPosts(nrOfBlogPostCards));
+    dispatch(fetchLatestPosts(nrOfBlogPostCards));
   }, []);
 
 
   return (
-    <Container fluid>
-      <Row>
-        <Col lg={8} md={6}>
-          <h4>LATEST THOUGHTS</h4>
-          <h1>Read Some of my latest blog posts.</h1>
+    <section id="blog" className={isDark ? styles.theme__dark : styles.theme__light}>
+      <Container fluid>
+        <Row>
+          <Col lg={6} md={5}>
+            <h4 className={styles.header}>LATEST THOUGHTS</h4>
+            <h1 className={styles.heading}>Read Some of my latest blog posts.</h1>
 
-          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus sed integer pulvinar quam scelerisque. Arcu hendrerit ut aliquet lorem vitae sit. Ut ornare sed iaculis blandit nulla. Habitant maecenas maecenas arcu auctor fermentum egestas urna.</h5>
-          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor lectus diam consequat, id amet enim. Auctor nulla tortor quisque risus. Et et, commodo at nec tellus eget fermentum vel. Enim vel morbi sed facilisi morbi urna, quam aliquam sed.</h5>
-        </Col>
-        <Col lg={4} md={6}>
-          {posts.map(({ title, summary, createdAt, slug }, idx) => (
-              <BlogPostCard key={idx} title={title} summary={summary} date={createdAt?.toLocaleString()} time="5 min" />
-          ))}
+            <h5 className={styles.content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus sed integer pulvinar quam scelerisque. Arcu hendrerit ut aliquet lorem vitae sit. Ut ornare sed iaculis blandit nulla. Habitant maecenas maecenas arcu auctor fermentum egestas urna.</h5>
+            <h5 className={styles.content}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor lectus diam consequat, id amet enim. Auctor nulla tortor quisque risus. Et et, commodo at nec tellus eget fermentum vel. Enim vel morbi sed facilisi morbi urna, quam aliquam sed.</h5>
+          </Col>
+          <Col lg={2} md={1} />
+          <Col lg={4} md={6}>
+            {posts.map(({ title, summary, createdAt, slug }, idx) => (
+              <div className={styles.blog_post_card}>
+                <BlogPostCard key={idx} title={title} summary={summary} date={createdAt?.toLocaleString()} time="5 min" />
+              </div>
+            ))}
 
-          {/* <BlogPostCard title="Some blog title" summary="summary" date="01/01/2021" time="10 min" />
+            {/* <BlogPostCard title="Some blog title" summary="summary" date="01/01/2021" time="10 min" />
           <BlogPostCard title="Some blog title" summary="summary" date="01/01/2021" time="10 min" />
           <BlogPostCard title="Some blog title" summary="summary" date="01/01/2021" time="10 min" />
           <BlogPostCard title="Some blog title" summary="summary" date="01/01/2021" time="10 min" /> */}
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
